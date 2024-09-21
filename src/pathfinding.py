9b1e3496f49fa6d7dc07dd2cc9d3b568829d434b
t000f <toffelaakso@gmail.com>
2024-09-21 18:46:05 +0300

    a*

--- src/pathfinding.py
+++ src/pathfinding.py
@@ -6,8 +6,8 @@ from ui import draw_grid
 
 DIRECTIONS = [
     (0, 1),  # right
-    (1, 0),  # down
     (0, -1),  # left
+    (1, 0),  # down
     (-1, 0),  # up
     # (-1, -1),  # up-left
     # (-1, 1),  # up-right
@@ -24,8 +24,62 @@ def create_grid(width, height, obstacles):
 
 
 def astar(grid, start, goal):
-    # TODO Implement a* algorithm
-    return [(i, i) for i in range(20)]  # Return an empty path if no path is found
+
+    # Manhattan distance heuristic
+    def heuristic(a, b):
+        return abs(b[0] - a[0]) + abs(b[1] - a[1])
+
+    # Get the neighbors of a node
+    def get_neighbors(node):
+        result = []
+        for direction in DIRECTIONS:
+            neighbor = (node[0] + direction[0], node[1] + direction[1])
+            if (
+                0 <= neighbor[0] < len(grid)
+                and 0 <= neighbor[1] < len(grid[0])
+                and grid[neighbor[0]][neighbor[1]] != 1
+            ):
+                result.append(neighbor)
+        return result
+
+    visited_nodes = set()  # Nodes already evaluated
+    unexplored_nodes = {start}  # Nodes to be evaluated
+    came_from = {}  # Previous node for reconstructing the path
+
+    g_score = {start: 0}  # Cost from start along best path
+    f_score = {start: heuristic(start, goal)}  # Estimated total cost from start to goal
+
+    while unexplored_nodes:
+        current = min(unexplored_nodes, key=lambda x: f_score[x])
+
+        if current == goal:
+            path = []
+            while current in came_from:
+                path.append(current)
+                current = came_from[current]
+            path.append(start)
+            return path[::-1]
+
+        unexplored_nodes.remove(current)
+        visited_nodes.add(current)
+
+        for neighbor in get_neighbors(current):
+            if neighbor in visited_nodes:
+                continue
+
+            # The distance from start to a neighbor
+            new_g_score = g_score[current] + 1
+
+            if neighbor not in unexplored_nodes:
+                unexplored_nodes.add(neighbor)
+            elif new_g_score >= g_score[neighbor]:
+                continue
+
+            came_from[neighbor] = current
+            g_score[neighbor] = new_g_score
+            f_score[neighbor] = g_score[neighbor] + heuristic(neighbor, goal)
+
+    return []  # No path found
 
 
 def jps(grid, start, goal):
